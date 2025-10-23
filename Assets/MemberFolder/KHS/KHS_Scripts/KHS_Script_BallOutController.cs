c9cdf1b096b67b0e135c4cd8fa22182970a82a79
Dr.kim[Academy] <dr7007@naver.com>
2025-10-23 17:03:19 +0900

    [Feat] [Add] : 현재 점수 UI표기 추가, 점수 계산을 위한 일부 스크립트 추가 [Update] : 장애물 및 일부 판정을 위한 스크립트 일부 수정.

---
+++ Assets/MemberFolder/KHS/KHS_Scripts/KHS_Script_BallOutController.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class KHS_Script_BallOutController : MonoBehaviour
+{
+    public static event Action BallOutEvt;
+
+    private void OnCollisionEnter(Collision _collision)
+    {
+        if (_collision.gameObject.name == "Ball")
+        {
+            Debug.LogError("BallOut!");
+            BallOutEvt?.Invoke();
+        }
+    }
+}
