71eb91a83915d05275b58dd240bc95cc3953bec8
Dr.kim[Academy] <dr7007@naver.com>
2025-10-24 17:48:11 +0900

    [Add] : 빌드 및 통합 작업을 위한 Main_Scene_로비 게임 추가 및 빌드 프로필 갱신

--- Assets/MemberFolder/KHS/KHS_Scripts/KHS_Script_ResetController.cs
+++ Assets/MemberFolder/KHS/KHS_Scripts/KHS_Script_ResetController.cs
@@ -5,15 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class KHS_Script_ResetController : MonoBehaviour
 {
+    
     public static event Action OnReset;
     public string gameSceneName;
+    public string lobbySceneName;
+
+    private bool isClear = false;
+
+    private void Start()
+    {
+        isClear = false;
+    }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        if(isClear)
         {
-            Debug.LogError("Reset!");
-            SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+            if(Input.anyKeyDown)
+            {
+                Debug.LogError("Go to Lobby");
+                SceneManager.LoadScene(lobbySceneName, LoadSceneMode.Single);
+            }
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Debug.LogError("Reset!");
+                SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        KHS_Script_ScoreManager.OnGameClear += GameClear;
+        KHS_Script_ScoreManager.OnGameOver += GameOver;
+    }
+
+    private void OnDisable()
+    {
+        KHS_Script_ScoreManager.OnGameClear -= GameClear;
+        KHS_Script_ScoreManager.OnGameOver -= GameOver;
+    }
+
+    private void GameOver()
+    {
+
+    }
+
+    private void GameClear()
+    {
+        isClear = true;
     }
 }
