231fccf6185f76ded99045ab3c70c1fceda56078
dr7007 <dr7007@naver.com>
2025-10-30 19:08:46 +0900

    [Update] : 일부 작업 수정 본 적용

---
+++ Assets/MemberFolder/KHS/KHS_Scripts/KHS_Script_BonusStageManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class KHS_Script_BonusStageManager : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+
+    private int bonusStageIdx = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bonusStageIdx = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
